2ea484c7178de9c1edd532effaf4d33f012279e0
ranma2913 <ranma2913@gmail.com>
2015-05-01 12:32:54 -0500

    Testing Git

--- app/src/main/java/com/ranma2913/pricecompare/HomeScreen.java
+++ app/src/main/java/com/ranma2913/pricecompare/HomeScreen.java
@@ -18,6 +18,7 @@ public class HomeScreen extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_home_screen, menu);
+        System.out.println("Test");
         return true;
     }
 
