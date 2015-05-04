94b5a10bc4295bcefea7a617442ed694b3158189
ranma2913 <ranma2913@gmail.com>
2015-05-04 18:00:42 -0500

    Added the text formatting for the currency input and AndroidAnnotations

--- app/src/main/java/com/ranma2913/pricecompare/HomeScreen.java
+++ app/src/main/java/com/ranma2913/pricecompare/HomeScreen.java
@@ -1,39 +1,72 @@
 package com.ranma2913.pricecompare;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.SystemService;
+import org.androidannotations.annotations.ViewById;
 
-public class HomeScreen extends ActionBarActivity {
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_screen);
-    }
+@EActivity(R.layout.activity_home_screen)
+public class HomeScreen extends Activity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home_screen, menu);
-        System.out.println("Test");
-        return true;
-    }
+    @ViewById(R.id.homeScreenWelcomeMessage)
+    TextView homeScreenWelcomeMessage;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    @ViewById(R.id.itemDescriptionInput)
+    EditText itemDescriptionInput;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    @ViewById(R.id.itemPriceInput)
+    EditText itemPriceInput;
+
+    @ViewById(R.id.numberOfUnitsInput)
+    EditText numberOfUnitsInput;
+
+    @ViewById(R.id.typeOfUnitsInput)
+    EditText typeOfUnitsInput;
+
+    @SystemService
+    InputMethodManager inputManager;
+    View.OnKeyListener typeOfUnitsInputKeyListener = new View.OnKeyListener() {
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            // If the event is a key-down event on the "enter" button
+            if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                // Perform action on key press
+                calculatePrice();//match this behavior to your 'Send' (or Confirm) button
+                return true;
+            }
+            return false;
         }
+    };
 
-        return super.onOptionsItemSelected(item);
+    @Click
+    void calculatePriceButton() {
+        calculatePrice();
+    }
+
+    private void calculatePrice() {
+        BigDecimal pricePerUnit = new BigDecimal(itemPriceInput.getText().toString().replace("$", "")).divide(new BigDecimal(numberOfUnitsInput.getText().toString()), MathContext.DECIMAL128);
+        String outputString = itemDescriptionInput.getText().toString()
+                + " $" + new BigDecimal(pricePerUnit.toString()).setScale(4, RoundingMode.HALF_UP).doubleValue()
+                + " Price Per Unit";
+        homeScreenWelcomeMessage.setText(outputString);
+
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();  // Always call the superclass method first
+        itemPriceInput.addTextChangedListener(new MoneyTextWatcher(itemPriceInput));
+        typeOfUnitsInput.setOnKeyListener(typeOfUnitsInputKeyListener);
     }
 }
